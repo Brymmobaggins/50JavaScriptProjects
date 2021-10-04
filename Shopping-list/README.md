760ed0ac4e2d4af8d9db30b75eff4289e3eaf212
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-10-04 04:19:04 +0100

    update shopping list

---
+++ Shopping-list/README.md
@@ -0,0 +1,17 @@
+# My Shopping list app
+
+## key concept
+
+- Dom manipulation
+- Events
+- Function blocks
+  
+## Preview
+
+![alt](img/Screen%20Shot%202021-10-03%20at%203.36.04%20AM.png)
+
+## Built with
+
+- HTML
+- CSS
+- JavaScript
--- Shopping-list/script.js
+++ Shopping-list/script.js
@@ -1,34 +1,26 @@
 const input = document.getElementById("item");
 const btn = document.querySelector("button");
 const ul = document.querySelector("ul");
-//   const para = document.querySelector("p");
 
 btn.addEventListener("click", function () {
     let myItem = input.value;
     input.value = "";
-
-    // if (myItem === "") {
-    // //   btn.disabled = true;
-    //   para.textContent = "Your item cannot be empty";
-    // //   ul.textContent = "";
-    // } else {
-    // //   para.textContent = "";
-    // }
-
-    // create an element that hold reference to the list
+    
     const listItem = document.createElement("li");
     const listText = document.createElement("span");
     const listBtn = document.createElement("button");
 
     listItem.appendChild(listText);
-    listText.textContent = myItem;
+    listText.textContent = input.value;
 
     listItem.appendChild(listBtn);
     listBtn.textContent = "Delete";
     ul.appendChild(listItem);
-
+     
+    // button click to when is to deleted
     listBtn.onclick = function (e) {
         ul.removeChild(listItem);
     };
     input.focus();
 });
+
