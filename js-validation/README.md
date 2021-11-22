ed988b749d50eb286dded3944e4584b223fb1bcb
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-11-22 15:07:37 +0100

    update project - modify JavaScript - fix errors on logic condition

--- js-validation/README.md
+++ js-validation/README.md
@@ -1,1 +1,9 @@
-# JavaScript Validation With Regex
+# JavaScript Validation with Regex
+
+for this project there are three required inputs for validation.
+
+- The first requires the user to enter letters with spaces which could be use as username
+
+- The second requires the user to  uppercase, lowercase number special character
+
+- The Third requires the user to enter email addresses on gmail. [(.....@gmail.com)](@gmail.com)
--- js-validation/script.js
+++ js-validation/script.js
@@ -5,39 +5,47 @@ form.addEventListener("submit", function (e) {
 });
 
 function checkValid() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  let email = document.getElementById("email").value;
-  let passRGEX = /^[A-Z]*$/; // five capital letters
-  let userRGEX = /^[A-Za-z]+$/; // only letters without spaces
-  let emailRGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // email validtion
+  let username = document.getElementById("user-name");
+  let password = document.getElementById("pass-word");
+  let email = document.getElementById("e-mail");
+  let userRGEX = /^[A-Za-z]+$/; // Regex for letters with no spaces
 
-  if (username === "") {
+  // Regex password uppercase lowercase number special characters
+  let passRGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]{8,}$/; 
+
+  if (username.value === "") {
     showError(username, "username cannot be empty");
-  } else if (username.match(userRGEX)) {
+  } else if (username.value.match(userRGEX)) {
     alert("Congratulation you passed!");
   } else {
-    showError(username, "username must have 5 uppercase letters");
+    showError(username, "username must have not spaces");
   }
-  if (password === "") {
+  
+  if (password.value === "") {
     showError(password, "Password cannot be empty");
-  } else if (password.match(passRGEX)) {
+  } else if (password.value.match(passRGEX)) {
     alert("Congratulation You Passed");
   } else {
-    showError(password, "Password should be letter only");
+    showError(password, "Password should have special chars");
   }
-  if (email === "") {
+  if (email.value === "") {
     showError(email, "Email cannot be empty");
-  } else if (email.test(emailRGEX)) {
-    alert("Successful Email");
-  } else {
+  } else if (!isValid(email.value)) {
     showError(email, "This is not a valid email");
+  } else {
+    alert("Successful Email");
   }
 }
+
 function showError(input, message) {
   let formControl = input.parentElement; // form-control
   let error = formControl.querySelector('small');
 
   error.textContent = message; // add error to message
-  formControl.className = 'form-control error'; // add error class
+  formControl.className = 'form-control error';
+}
+
+function isValid(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
