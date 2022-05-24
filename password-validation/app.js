5e724dac57dddaf56ec38b45b6aa5d00f102aa45
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-05-24 03:16:19 +0100

    complete js code

--- password-validation/app.js
+++ password-validation/app.js
@@ -1,6 +1,9 @@
 const password = document.getElementById('pass-word')
 const letter = document.getElementById('letter')
 const messageBox = document.getElementById('message')
+const capital = document.getElementById('capital')
+const length = document.getElementById('length')
+const number = document.getElementById('number')
 
 // whenevers user clicks on the password field, show the message box
 password.onfocus = function () {
@@ -12,7 +15,7 @@ password.onblur = function () {
     messageBox.style.display = "none"
 
 }
-
+// when user starts to type in the password field
 password.onkeyup = function () {
     // Validate lowercase
     let lowerCaseLetters = /[a-z]/g;
@@ -24,4 +27,36 @@ password.onkeyup = function () {
         letter.classList.remove('valid')
         letter.classList.add("invalid")
     }
+    // validate capital letters
+    let upperCaseLetters = /[A-Z]/g;
+    if (password.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid")
+        capital.classList.add("valid")
+    } else {
+        capital.classList.remove("valid")
+        capital.classList.add("invalid")
+    }
+    // validate Number
+    let numbers = /[0-9]/g
+
+    if (password.value.match(numbers)) {
+        number.classList.remove("invalid")
+        number.classList.add("valid")
+
+    } else {
+        number.classList.remove("valid")
+        number.classList.add("invalid")
+
+    }
+    // validate Length
+    if (password.value.length >= 8) {
+        length.classList.remove("invalid")
+        length.classList.add("valid")
+
+    } else {
+        length.classList.remove("valid")
+        length.classList.add("invalid")
+
+    }
+
 }
