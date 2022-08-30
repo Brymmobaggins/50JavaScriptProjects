6165e086e1ee9ea2ae82d73082bf330875509a06
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-30 18:02:36 +0100

    create readme and update passenegsenger counter app

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -3,14 +3,14 @@ const countEl = document.getElementById('count-el')
 const saveEl = document.getElementById('save-el')
 let count = 0
 
-console.log(saveEl)
 
+// function to run whenever the incremnent button is clicked
 function increment(){
     count += 1
     countEl.textContent = count
 }
 
-
+// function to run whenever the save button is clicked
 function save(){
     let countStr = count + " - "
     saveEl.textContent += countStr
---
+++ passenger-counter/readme.md
@@ -0,0 +1,22 @@
+# Passenger counter App
+
+
+## Preview
+
+![alt](../passenger-counter/img/Screen%20Shot%202022-08-30%20at%205.35.43%20PM.png)
+
+## links
+
+[live Demo]()
+
+## Built with
+
+- Html
+- CSS
+- JavaScript
+  
+## key concept
+
+- DOM
+- function
+- Math fundamental
