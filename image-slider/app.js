853d3a374d2d898561e3ebd57022e338852cdfda
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-12-29 21:58:12 +0100

    add css to image slider

---
+++ image-slider/app.js
@@ -0,0 +1,8 @@
+const pictures = ["Rabbit-1.jpg", "snake-2.jpg", "cat-3.jpg", "Dog-4.jpg", "monkey-5.jpg", "peacock-6.jpg", "owl-7.jpg", "food-8.jpg", "culture-9.jpg", "egypt-10.jpg"];
+
+
+
+for (let i = 0; i < pictures.length; i++) {
+    
+
+}
