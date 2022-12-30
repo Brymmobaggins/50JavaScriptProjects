cd2c776b781aacbbe6f98aa1146e3b046f444f95
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-12-30 22:14:14 +0100

    add javascript code for interaction

--- image-slider/app.js
+++ image-slider/app.js
@@ -1,8 +1,35 @@
-const pictures = ["Rabbit-1.jpg", "snake-2.jpg", "cat-3.jpg", "Dog-4.jpg", "monkey-5.jpg", "peacock-6.jpg", "owl-7.jpg", "food-8.jpg", "culture-9.jpg", "egypt-10.jpg"];
 
 
+(function () {
 
-for (let i = 0; i < pictures.length; i++) {
-    
+    // array of pictures
+    const pictures = ["Rabbit-1", "snake-2", "cat-3", "Dog-4", "monkey-5", "peacock-6", "owl-7", "food-8", "culture-9", "egypt-10"];
 
-}
+
+    // select button left right and add event listeners
+    const buttons = document.querySelectorAll(".btn")
+    const imgDiv = document.getElementById('img-container')
+    let counter = 0;
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            if (button.classList.contains('prev')) {
+                counter--
+                if (counter < 0) {
+                    counter = pictures.length - 1
+                }
+                imgDiv.style.backgroundImage = `url('img/${pictures[counter]}.jpg')`
+
+            }
+            if (button.classList.contains('next')) {
+                counter++
+                if (counter > pictures.length - 1) {
+                    counter = 0
+                }
+                imgDiv.style.backgroundImage = `url('img/${pictures[counter]}.jpg')`
+
+            }
+        }
+        )
+    });
+})()
