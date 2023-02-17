da1f2df8771b4fc45d687a7cd5e59da722fc1fb9
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-02-17 15:54:03 +0100

    update counter app project

--- counter-app/main.js
+++ counter-app/main.js
@@ -1,15 +1,15 @@
 
-// reference the 
+// reference the the element that show the count
 const display = document.querySelector('#show-count')
 
 // reference all the buttons
 const allBtns = document.querySelector('#allBtns')
-let value = 0
 
 // Whenever any of the buttons is click
 allBtns.addEventListener('click', counter)
 
 // function that runs whenever the buttons is clicked
+let value = 0
 function counter(e) {
     const btn = e.target.id
     if (btn === 'increase') {
@@ -20,6 +20,6 @@ function counter(e) {
         display.textContent = value
     } else {
         value = 0
+        display.textContent = value
     }
-    display.textContent = value
 }
