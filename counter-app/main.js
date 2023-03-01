feaac55a80bd2f40230d689140a65d003776c1b4
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-03-01 06:00:34 +0100

    refactor and add more features

--- counter-app/main.js
+++ counter-app/main.js
@@ -8,18 +8,41 @@ const allBtns = document.querySelector('#allBtns')
 // Whenever any of the buttons is click
 allBtns.addEventListener('click', counter)
 
-// function that runs whenever the buttons is clicked
 let value = 0
+
+// function that for count color
+function showRed() {
+    if (value <= 0) {
+        display.style.color = 'Red'
+    } else {
+        display.style.color = 'white'
+    }
+
+}
+
+
+// function that runs whenever the buttons is clicked
 function counter(e) {
+    // variable that target the id of every button
     const btn = e.target.id
+
+    // if id is `increase` count increases
     if (btn === 'increase') {
         value++
         display.textContent = value
+        showRed()
+
+        // if the id value is "decrease" count decreases
     } else if (btn === 'decrease') {
         value--
         display.textContent = value
+        showRed()
+
+        // if it is otherwise count goes back to zero
     } else {
         value = 0
         display.textContent = value
     }
 }
+
+
