772282e558b9f76e0e5cdb45d4c6c81318d59cce
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-04-05 09:30:18 +0100

    complete dollar to naira project

--- dollar-to-Naira/coverter.js
+++ dollar-to-Naira/coverter.js
@@ -5,15 +5,21 @@ let convertBtn = document.querySelector('#convert')
 function convertToDollar() {
     // convert dollar input value to number
     dollarAmount = Number(dollarAmount.value)
+    
 
     // Check if dollar amount is number, less than Zero or empty
     if (isNaN(dollarAmount) || dollarAmount <= 0 || dollarAmount === null) {
-        alert("Please enter a Dollar amount")
+        alert("Please enter a Dollar Amount")
     }
     // otherwise do the calculation
     else {
         let calculatedAmount = dollarAmount * 750
         nairaAmount.value = "N" + calculatedAmount
+
     }
 
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
