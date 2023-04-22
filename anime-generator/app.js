c81103ff7ea5e7ee8df9c40e4156eb4325685232
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-04-22 14:24:42 +0100

    fix add script code

--- anime-generator/app.js
+++ anime-generator/app.js
@@ -4,11 +4,12 @@ const animeButton = document.getElementById('anime-btn')
 
 animeButton.addEventListener('click', getAnime)
 
-
+// using fetch to retrieve data 
 function getAnime() {
     fetch('https://nekos.best/api/v2/neko')
         .then(response => response.json())
         .then(data => {
-            animeContainer.innerHTML = `<img src="${data.results[0].url}"/> `;
+            animeContainer.innerHTML = `<img src="${data.results[0].url}" lazy="loading" />`;
         })
 }
+
