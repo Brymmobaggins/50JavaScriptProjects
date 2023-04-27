ec51b88d05684c06105f021f9e3363a25e82f1ea
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-04-27 22:29:20 +0100

    rename project name

---
+++ roman-to-numeral/app.js
@@ -0,0 +1,40 @@
+
+const convertButton = document.getElementById('convert')
+
+convertButton.addEventListener('click', romanToNumber)
+
+
+function romanToNumber() {
+    let numInput = document.getElementById('roman-value').value
+
+    var roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+
+    let result = document.getElementById("show-roman")
+    let num = 0
+
+    for (let i = 0; i < numInput.length; i++) {
+        let current = roman[numInput[i]];
+        let next = roman[numInput[i + 1]];
+
+        if (current < next) {
+            num -= next - current;
+            // i++
+        } else {
+            num += current;
+        }
+
+    }
+    result.innerHTML = `<h1>sss ${num}</h1>`
+
+    // console.log(result)
+}
+
