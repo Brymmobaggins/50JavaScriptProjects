5a92bf95d3dc4972adc79aee7af268ba94d10862
brymmobaggins <ibro2tablet21@yahoo.com>
2023-08-07 04:45:11 +0100

    validation conditon

--- word-count/app.js
+++ word-count/app.js
@@ -6,26 +6,35 @@ form.addEventListener('submit', function (e) {
     countWord()                // invoke the `countWord` function 
 })
 
+// clear input
+userInput.onfocus = function () {
+    userInput.textContent = ""
+    wordCountDisplay.textContent = ""
+
+}
 
 function countWord() {
     const wordCountDisplay = document.querySelector('p');
     const userInput = document.getElementById('string').value;
     const characterCount = userInput.length;
-  
+
     // condition to check if the input is empty
     if (userInput === "") {
-      wordCountDisplay.textContent = "Your input cannot be empty";
-      wordCountDisplay.classList.add("error");
-      setTimeout(function() {
-        wordCountDisplay.textContent = " ";
-      }, 2000);
+        wordCountDisplay.textContent = "Your input cannot be empty";
+        wordCountDisplay.classList.add("error");
+        setTimeout(function () {
+            wordCountDisplay.textContent = " ";
+        }, 2000);
     } else {
-      wordCountDisplay.innerHTML = `<h1>You entered ${characterCount} characters</h1>`;
-      wordCountDisplay.classList.remove("error");
-      setTimeout(function() {
-        wordCountDisplay.textContent = "";
-      }, 2000);
+        wordCountDisplay.innerHTML = `<h2>You entered ${characterCount} character</h2>`;
+        wordCountDisplay.classList.remove("error");
+        setTimeout(function () {
+            wordCountDisplay.textContent = "";
+        }, 2000);
+        if(characterCount > 1){
+            wordCountDisplay.innerHTML = `<h1>You entered ${characterCount} characters</h1>`;
+        }
     }
-  }
-  
+}
+
 
