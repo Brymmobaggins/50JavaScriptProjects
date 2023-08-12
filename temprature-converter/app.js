e639801d5eb36505ec40c05cf88b6c8b8ea0fd38
brymmobaggins <ibro2tablet21@yahoo.com>
2023-08-12 02:12:19 +0100

    remove unnessary comments and blak lines

--- temprature-converter/app.js
+++ temprature-converter/app.js
@@ -3,15 +3,13 @@ const convertButton = document.getElementById('convert-btn');
 const resultElement = document.getElementById('result');
 const measureSelect = document.getElementById('measure')
 
-
-
 // Celsius conversion funtion
 function convertToCelsius(fahrenheit) {
     return ((fahrenheit - 32) * 5 / 9)
 }
 
 // fahrenheit conversion function
-function convetToFahrenheit(celsius) {
+function convertToFahrenheit(celsius) {
     return ((celsius * 1.8)) + 32
 }
 
@@ -26,17 +24,21 @@ const handleConvert = () => {
         conversionResult = ""
     } else if (Number.isNaN(inputValue)) {
         alert("Please enter a valid number");
-        conversionResult = ""
     } else {
 
-
-        // use ternary for simple condition
-        conversionResult = measureSelect.value === 'celsius' ? convertToCelsius(inputValue) : convetToFahrenheit(inputValue)
+        // ternary of simple condition
+        conversionResult =
+            measureSelect.value === 'celsius'
+                ? convertToCelsius(inputValue)
+                : convertToFahrenheit(inputValue);
     }
-    resultElement.value = conversionResult
 
-}
-convertButton.onclick = handleConvert
+    resultElement.value = conversionResult;
+
+};
+
+convertButton.onclick = handleConvert;
+
 
 
 
---
+++ temprature-converter/readme.md
@@ -0,0 +1,42 @@
+# Temperature converter 
+This is a simple temperature conversion program that converts from Fahrenheit to Celsius
+and vice versa. It also has the option of converting between Kelvin, Rankine or Re  
+amur.
+
+The user can choose which unit they want their input in and it will convert
+accordingly.  The result will be dispay on the result text box
+
+![alt](https://)
+
+.
+## Built with
+
+- HTML
+- CSS 
+- Javascript ES5
+
+## featurs
+- User chooses what type of temperatue they want to convert into (F
+,C,K,R)
+
+## concept
+ The main concept used include:
+ - DOM manipulation
+- Event listeners
+- Number functions `Number()` that converts values to integer 
+- Conditional statements
+- `if/else` to check input and output units
+- Arrow Functions
+
+
+
+
+
+
+
+
+
+
+
+
+
