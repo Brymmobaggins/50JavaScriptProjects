66bdf17f542c9f0bf5032d4ff23783dc67fd43b9
brymmobaggins <ibro2tablet21@yahoo.com>
2023-08-14 12:09:29 +0100

    add tempratute measurement symbol and fix validation

--- temprature-converter/app.js
+++ temprature-converter/app.js
@@ -1,42 +1,53 @@
-const inputTemperature = document.getElementById('degree')
+let inputTemperature = document.getElementById('degree')
 const convertButton = document.getElementById('convert-btn');
 const resultElement = document.getElementById('result');
-const measureSelect = document.getElementById('measure')
+let measureSelect = document.getElementById('measure')
+
+// temperature measurent symbols
+const fahrenheitSymbol = '°F'
+const celsiusSymbol = '°C';
+
+
 
 // Celsius conversion funtion
-function convertToCelsius(fahrenheit) {
-    return ((fahrenheit - 32) * 5 / 9)
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9
 }
 
 // fahrenheit conversion function
-function convertToFahrenheit(celsius) {
-    return ((celsius * 1.8)) + 32
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 1.8) + 32.
 }
 
-// click event 
+
+
 const handleConvert = () => {
     // convert input to number
-    const inputValue = Number(inputTemperature.value)
+    let inputValue = Number(inputTemperature.value)
+    let choice = measureSelect.value
     let conversionResult
-    //  validate input
+
+    //  check validate input
     if (inputValue == "") {
-        alert("Please enter a temperature")
+        alert("Please enter a value")
         conversionResult = ""
     } else if (Number.isNaN(inputValue)) {
         alert("Please enter a valid number");
+        conversionResult = ""
     } else {
 
         // ternary of simple condition
         conversionResult =
-            measureSelect.value === 'celsius'
-                ? convertToCelsius(inputValue)
-                : convertToFahrenheit(inputValue);
+            choice == 'celsius'
+                ? celsiusToFahrenheit(inputValue).toFixed(2) + fahrenheitSymbol
+                : fahrenheitToCelsius(inputValue).toFixed(2) + celsiusSymbol;
     }
 
     resultElement.value = conversionResult;
 
 };
 
+//  handleCovert funcion to execute when the button in click
 convertButton.onclick = handleConvert;
 
 
