32cb9487df22c86aac25761eb270692358c5e9c6
brymmobaggins <ibro2tablet21@yahoo.com>
2023-08-16 12:31:49 +0100

    update js code

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -1,11 +1,45 @@
-const binaryInput = document.getElementById('binary')
-const button = document.querySelector('#btn')
+let binaryInput = document.getElementById('binary-input')
+let errorMessage = document.getElementById('error-message')
+let decimalInput = document.getElementById("decimal-input")
 
 
-button.addEventListener("click", () => {
+// convert decimal to binary when user input the binary field
+decimalInput.addEventListener('input', function () {
 
-    const inputValue = parseInt(binaryInput.value, 2)
-    console.log(inputValue)
+    // convert input to a number
+    let decValue = parseInt(decimalInput.value)
+
+    if (decValue < 0 || isNaN(decValue)) {
+        errorMessage.textContent = "Please a valid decimal input"
+    } else {
+        // clear error message and set it back after conversion
+        errorMessage.textContent = ""
+        binaryInput.value = decValue.toString(2)
+
+    }
+})
+
+binaryInput.addEventListener("input", function () {
+
+    let binaryValue = binaryInput.value
+
+    if (validateBinary(binaryValue)) {
+        decimalInput.value = parseInt(binaryValue, 2)
+        errorMessage.textContent = ""
+    } else {
+        errorMessage.textContent = "Please enter a valid binary input"
+    }
+
+
+
+    function validateBinary(num) {
+        for (let i = 0; i < num.length; i++) {
+            if (num[i] != "1" && num[i] != "0") {
+                return false
+            } 
+        }
+        return true
+    }
 
 
 })
