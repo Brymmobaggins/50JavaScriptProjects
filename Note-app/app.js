22513241bbdae833180a3963657a09ff11f0a5ff
brymmobaggins <ibro2tablet21@yahoo.com>
2024-02-22 15:08:04 +0100

    create note app project

---
+++ Note-app/app.js
@@ -0,0 +1,10 @@
+
+const openModalBtn = document.querySelector('#open-modal')
+const overlay = document.querySelector('.over-lay')
+
+
+openModalBtn.addEventListener('click', function () {
+    overlay.className.remove('modal')
+
+
+})
