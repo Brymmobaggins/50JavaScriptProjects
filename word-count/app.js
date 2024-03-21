d793af14392e522da69f827364593ea408e1bdbf
brymmobaggins <ibro2tablet21@yahoo.com>
2024-03-21 07:55:43 +0100

    refactor code

--- word-count/app.js
+++ word-count/app.js
@@ -3,46 +3,54 @@ const wordCountResult = document.querySelector("p");
 const countContainer = document.querySelector(".container");
 let inputEl = document.querySelector("input");
 
+
 function countWord() {
-  let characterCount = inputEl.value.length;
+    let characterCount = inputEl.value.length;
 
-  // condition to check if the input is empty
-  if (!inputEl.value) {
-    showErrorMessage();
-  } else {
-    showCharacterCount(characterCount);
-    saveToLocalStorage(characterCount);
-  }
-  clearInput();
+    // condition to check if the input is empty
+    if (!inputEl.value) {
+        showErrorMessage();
+    } else if (!isNaN(inputEl.value)) {
+         alert("invalid input")
+    } else {
+        showCharacterCount(characterCount);
+        saveCountToLocalStorage(characterCount);
+    }
+    clearInput();
 }
 
+
+// function to show error message
 function showErrorMessage() {
-  wordCountResult.textContent = "Your input cannot be empty";
-  wordCountResult.classList.add("error");
-  setTimeout(() => {
-    wordCountResult.textContent = " ";
-  }, 2000);
+    wordCountResult.textContent = "Your input cannot be empty";
+    wordCountResult.className = "error";
+
+    // vanish in two seconds
+    setTimeout(() => {
+        wordCountResult.textContent = " ";
+    }, 2000);
 }
 
+// function to show character count
 function showCharacterCount(characterCount) {
-  if (characterCount > 1) {
-    wordCountResult.innerHTML = `You entered ${characterCount} characters`;
-  } else {
-    wordCountResult.innerHTML = `You entered ${characterCount} character`;
-  }
-  wordCountResult.classList.remove("error");
-  setTimeout(() => {
-    wordCountResult.textContent = "";
-  }, 2000);
+    if (characterCount > 1) {
+        wordCountResult.innerHTML = `You entered ${characterCount} characters`;
+    } else {
+        wordCountResult.innerHTML = `You entered ${characterCount} character`;
+    }
+    wordCountResult.classList.remove("error");
+    setTimeout(() => {
+        wordCountResult.textContent = "";
+    }, 3000);
 }
 
 //  function to save local storage of 
-function saveToLocalStorage(characterCount) {
+function saveCountToLocalStorage(characterCount) {
     localStorage.setItem("wordCount", characterCount)
 
 }
 
 //  function to clear input
 function clearInput() {
-  inputEl.value = "";
+    inputEl.value = "";
 }
