79c777c1c89c6723be4951cebb579eab288f34c5
brymmobaggins <ibro2tablet21@yahoo.com>
2024-06-21 20:52:45 +0100

    refactor code

--- vowel-counter/app.js
+++ vowel-counter/app.js
@@ -1,39 +1,9 @@
-const vowelCountShow = document.getElementById('show')
-const form = document.getElementById('form')
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault()                           // prevent default submit
-    if (userInput.value === "") {
-        vowelCountShow.textContent = "Please enter some words"
-    } else if (isNaN(userInput.value)) {
-        checkVowel()
-    } else {
-        vowelCountShow.textContent = "Numbers are not allowed"
 
-    }
+const form = document.querySelector("form")
 
-})
-// clear input
-input.onfocus = function () {                     // when input is focus 
-    vowelCountShow.textContent = ""               // clear text
-    input.value = ""                              // clear input value
-}
-// check vowel
-let vowelCount = 0;                                // create variable that holds the count 
-function checkVowel() {
-    const userInput = document.getElementById('input').value // get the value with an input
-    const vowels = ["a", "e", "i", "o", "u"]       // an array of vowel letters
-    for (let i = 0; i < userInput.length; i++) {      // loop through the input value
-        for (let j = 0; j < vowels.length; j++) {   // loop through the array of vowel letters
-            if (input[i] === vowels[j]) { // if statement to check if each iteration of input is the same as each iteration of vowel
-                vowelCount++                 // if yes, count increases by 1
-            }
-        }
-    }
-    if (vowelCount > 1) {
-        vowelCountShow.innerHTML = `${userInput} have ${vowelCount} vowel letters`;
-    } else {
-        vowelCountShow.innerHTML = `${userInput} has ${vowelCount} vowel letter`;
+// pevent default form submition
+form.onsubmit = function (e) {
+    e.preventDefault();
 
-    }
 }
