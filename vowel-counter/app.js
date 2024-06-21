e48483820fa8d82b94b623dafbfccdfb586e383a
brymmobaggins <ibro2tablet21@yahoo.com>
2024-06-21 22:53:42 +0100

    create function to check for vowel letter in a string

--- vowel-counter/app.js
+++ vowel-counter/app.js
@@ -1,9 +1,40 @@
 
-
 const form = document.querySelector("form")
+const showResult = document.querySelector("#show")
 
-// pevent default form submition
+// pevent default form submit
 form.onsubmit = function (e) {
     e.preventDefault();
+    countVowel()
+}
+
+// function to count the vowel letter in a strings of letters
+function countVowel() {
+    const input = document.querySelector("input").value.trim()
+    const vowels = "aeiou";
+    let count = 0;
+    for (let char of input) {
+        if (vowels.includes(char.toLowerCase())) {
+            count++;
+        }
+    }
+
+    if (input == "") {
+        showResult.textContent = `Please enter a word`;
+    } else if (count > 1) {
+        showResult.textContent = `${input} have ${count} vowels`;
+    } else {
+        showResult.textContent = `${input} has ${count} vowel`;
+    }
+    clearUp()
+}
+
+// clear UI after three seconds
+function clearUp() {
+    setTimeout(function () {
+        showResult.textContent = "";
+        input.value = "";
+    }, 3000);
 
 }
+
