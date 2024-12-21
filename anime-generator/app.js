67ff5b34780f6a581d3e80350bf51d77a56e5965
brymmobaggins <ibro2tablet21@yahoo.com>
2024-12-21 18:50:38 +0100

    refactor project structure

--- anime-generator/app.js
+++ anime-generator/app.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const animeContainer = document.getElementById("anime-result");
 const animeButton = document.getElementById("anime-btn");
 
@@ -7,19 +9,18 @@ document.addEventListener("DOMContentLoaded", getAnime);
 // Add event listener to the button
 animeButton.addEventListener("click", getAnime);
 
-// Function to fetch anime data from the API
+// Function to fetch anime data from the API and update the HTML content with the fetched data
 async function getAnime() {
-    try {
-        const response = await fetch("https://nekos.best/api/v2/neko");
-        const data = await response.json();
-        // Update the HTML content with the fetched data
-        animeContainer.innerHTML = 
-        `
+  try {
+    const response = await fetch("https://nekos.best/api/v2/neko");
+    const data = await response.json();
+    // Update the HTML content with the fetched data
+    animeContainer.innerHTML = `
         <h3><strong>Name:</strong> ${data.results[0].artist_name}</h3>
         <h4></h4>
         <img src="${data.results[0].url}" lazy="loading" />
         `;
-    } catch (error) {
-        console.error("Error fetching anime data:", error);
-    }
+  } catch (error) {
+    console.error("Error fetching anime data:", error);
+  }
 }
