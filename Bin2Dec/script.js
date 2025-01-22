69487e60de79ab0c82949920a14fd0f7d17a0d57
brymmobaggins <ibro2tablet21@yahoo.com>
2025-01-22 18:40:01 +0100

    specify files and folder to be ignored

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -1,41 +1,43 @@
+/** @format */
+
 let binaryNumberInput = document.getElementById("binary-input");
 let errorMessageDisplay = document.getElementById("error-message");
 let decimalNumberOutput = document.getElementById("decimal-input");
 
 // convert decimal to binary when user input the binary field
 decimalNumberOutput.addEventListener("input", function () {
-    // get decimal value entered
-    const decimalValue = parseInt(decimalNumberOutput.value);
+  // get decimal value entered
+  const decimalValue = parseInt(decimalNumberOutput.value);
 
-    // convert to binary and update binary input field
-    binaryNumberInput.value = decimalValue.toString(2);
+  // convert to binary and update binary input field
+  binaryNumberInput.value = decimalValue.toString(2);
 });
 
 binaryNumberInput.addEventListener("input", function () {
-    // get user input
-    const binaryValue = binaryNumberInput.value;
+  // get user input
+  const binaryValue = binaryNumberInput.value;
 
-    // validate input
-    if (isValidBinary(binaryValue)) {
-        // convert to decimal and output
-        decimalNumberOutput.value = parseInt(binaryValue, 2);
-        errorMessageDisplay.textContent = "";
-    } else {
-        //  show error message
-        errorMessageDisplay.textContent = "Please enter a valid binary number";
-    }
+  // validate input
+  if (isValidBinary(binaryValue)) {
+    // convert to decimal and output
+    decimalNumberOutput.value = parseInt(binaryValue, 2);
+    errorMessageDisplay.textContent = "";
+  } else {
+    //  show error message
+    errorMessageDisplay.textContent = "Please enter a valid binary number";
+  }
 
-    // validate binary input
-    function isValidBinary(binaryStr) {
-        // loop through each character in input
-        for (let char of binaryStr) {
-            // check if character is 0 or 1
-            if (char !== "0" && char !== "1") {
-                // if character is not 0 or 1, return false
-                return false;
-            }
-        }
-        // if all characters are 0 or 1, return true
-        return true;
+  // validate binary input
+  function isValidBinary(binaryStr) {
+    // loop through each character in input
+    for (let char of binaryStr) {
+      // check if character is 0 or 1
+      if (char !== "0" && char !== "1") {
+        // if character is not 0 or 1, return false
+        return false;
+      }
     }
+    // if all characters are 0 or 1, return true
+    return true;
+  }
 });
--- consonant-counter/script.js
+++ consonant-counter/script.js
@@ -1,22 +1,25 @@
+/** @format */
 
 function countConsonant() {
-    const inputValue = document.getElementById("input").value.trim();
-    let p = document.querySelector("p");
-    const consonantLetters = "bdcfghjklmnpqrstvwxyz";
-    let count = 0
+  const inputValue = document.getElementById("input").value.trim();
+  let p = document.querySelector("p");
+  const consonantLetters = "bdcfghjklmnpqrstvwxyz";
+  let count = 0;
 
-    for (let char of inputValue) {
-        if (consonantLetters.includes(char.toLowerCase())) {
-            count++
-        }
+  for (let char of inputValue) {
+    if (consonantLetters.includes(char.toLowerCase())) {
+      count++;
     }
+  }
 
-    if (count > 1) {
-        p.textContent = `${inputValue} has ${count} consonants`;
-    } else {
-        p.textContent = `${inputValue} has ${count} consonant`;
-    }
-    // disappear after 3 seconds
-    setTimeout(() => { p.textContent = ""; input.value = ""; }, 3000);
+  if (count > 1) {
+    p.textContent = `${inputValue} has ${count} consonants`;
+  } else {
+    p.textContent = `${inputValue} has ${count} consonant`;
+  }
+  // disappear after 3 seconds
+  setTimeout(() => {
+    p.textContent = "";
+    input.value = "";
+  }, 3000);
 }
-
