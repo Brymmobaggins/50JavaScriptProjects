f30de5d164ff7c68c7d37d37e243cbd732c720df
brymmobaggins <ibro2tablet21@yahoo.com>
2025-08-24 20:47:57 +0100

    fix project

--- github-user-fetcher/script.js
+++ github-user-fetcher/script.js
@@ -1,41 +1,43 @@
 /** @format */
 
 document.addEventListener("DOMContentLoaded", () => {
-  async function fetchUserProfile() {
+  async function fetchUserProfile(username) {
     try {
-      const response = fetch("https://api.github.com/users/${username}");
+      const response = await fetch(`https://api.github.com/users/${username}`);
       if (!response.ok) {
         throw new Error("User not found");
       }
-      //   convert fetch data to JSON and assign it to a variable called `data`
-      const data = (await response).json();
+      // convert fetch data to JSON and assign it to a variable called `data`
+      const data = response.json();
 
       return data;
     } catch (error) {
-
-     // catch error in the devtools
+      // catch error in the devtools
       console.error("Error fetching user:", error);
       return null;
     }
   }
 
   async function displayUserProfile() {
-    const username = document.getElementById("input").value;
+    const usernameInput = document.getElementById("input").value;
     const output = document.getElementById("output");
 
-    if (!username) {
-      output.innerHTML = `<p>Please enter user github username</p>`;
+    if (!usernameInput) {
+      output.innerHTML = `<p style="text-align:center; color: red">Please enter github username</p>`;
       return;
     }
-    output.innerHTML = `<p>fetching User profile</p>`;
+    output.innerHTML = `<p>Fetching user profile</p>`;
 
-    const user = await fetchUserProfile(username);
+    const user = await fetchUserProfile(usernameInput);
 
     if (user) {
       output.innerHTML = `
-      div>
-         <h3>${user.message || "Not available"}</h3>
-         <p>${user.status}</p>
+      <div style="">
+         <img src="${user.avatar_url}" width="100" style="border-radius: 50%" />
+         <p>${user.name || "Not available"}</p>
+         <p>${user.bio || "No bio provided"}</p>
+         <p>Repo: ${user.public_repos}</p>
+         <p>Followers: ${user.followers} || ${user.following} following </p>
       </div>
         `;
     } else {
@@ -45,4 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("fetchBtn")
     .addEventListener("click", displayUserProfile);
+  document
+    .getElementById("input")
+    .addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        displayUserProfile();
+      }
+    });
 });
